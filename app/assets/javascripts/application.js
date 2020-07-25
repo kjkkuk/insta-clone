20f95a7ab379f5685dbd81ae99c4f8c736d30ee8
kjkkuk <kjk.kuk@mail.ru>
2020-07-25 15:44:50 +0300

    init project, add bare and registration with devise

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,18 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//= require jquery.min
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require bootstrap.min
+//= require_tree .
